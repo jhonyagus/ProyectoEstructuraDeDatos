c26a3c7b337e4acaf1d1e0ad3d87d94290d37b02
unknown <artoapanta3@espe.edu.ec>
2019-11-06 02:07:11 -0500

    Nuevo Proyecto

---
+++ SudokuBactraking/Operaciones.h
@@ -0,0 +1,24 @@
+/***********************************************************************
+ * Module:  Operaciones.h
+ * Author:  RODRIGO
+ * Modified: miércoles, 6 de noviembre de 2019 01:54:07
+ * Purpose: Declaration of the class Operaciones
+ ***********************************************************************/
+
+#if !defined(__Class_Diagram_1_Operaciones_h)
+#define __Class_Diagram_1_Operaciones_h
+
+class Operaciones
+{
+public:
+   virtual bool Sudoku(int** tablero)=0;
+   virtual bool UtilizadoEnFila(int** tablero, int fila, int num)=0;
+   virtual bool UtilizadoEnColumna(int** tablero, int col, int num)=0;
+   virtual bool UtilizadoEnSudoku(int** tablero, int inicioFila, int inicioColumna, int num)=0;
+
+protected:
+private:
+
+};
+
+#endif
---
+++ SudokuBactraking/Tabla.cpp
@@ -0,0 +1,77 @@
+/***********************************************************************
+ * Module:  Tabla.cpp
+ * Author:  RODRIGO
+ * Modified: miércoles, 6 de noviembre de 2019 02:01:52
+ * Purpose: Implementation of the class Tabla
+ ***********************************************************************/
+
+#include "Tabla.h"
+
+
+bool Tabla::buscarLugar(int** tablero, int& fila, int& col)
+{
+
+}
+
+
+boll Tabla::comprobar(int** tablero, int fila, int col, int num)
+{
+
+}
+
+void Tabla::mostrar(int** tablero)
+{
+
+}
+
+
+int** Tabla::getTablero(void)
+{
+   return tablero;
+}
+
+
+void Tabla::setTablero(int** newTablero)
+{
+   tablero = newTablero;
+}
+
+
+Tabla::Tabla()
+{
+
+}
+
+
+Tabla::~Tabla()
+{
+
+}
+
+
+bool Tabla::Sudoku(int** tablero)
+{
+
+}
+
+
+bool Tabla::UtilizadoEnFila(int** tablero, int fila, int num)
+{
+
+
+}
+
+
+bool Tabla::UtilizadoEnColumna(int** tablero, int col, int num)
+{
+
+
+
+}
+
+
+bool Tabla::UtilizadoEnSudoku(int** tablero, int inicioFila, int inicioColumna, int num)
+{
+
+
+}
