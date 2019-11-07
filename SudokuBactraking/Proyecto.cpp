9c74d0eb292f2d4a1eb466252313efe4e2f9eda4
jhonyagus <jhonyagus@hotmail.com>
2019-11-06 22:51:15 -0500

    ayuda

--- SudokuBactraking/Proyecto.cpp
+++ SudokuBactraking/Proyecto.cpp
@@ -7,9 +7,20 @@
 #include <iostream>
 #include<stdlib.h>
 #include "Tabla.cpp"
+#include "ingreso.h"
 using namespace std;
 int main()
 {
+    Ingreso lee;
+    int opc;
+    do{
+        cout<<"Bienvenid@\n"<<"Escoja la opcion que desea realizar"<<endl;
+        cout<<"1. Mostrar solucion sudoku\n2.Mostrar ayuda del programa";
+        opc = lee.ingresarInt("\n---> ");
+    }while(opc != 1 && opc != 2 );
+    if(opc==2)
+       system("ayuda.chm");
+    if(opc==1){
     Tabla t = Tabla();
     if (t.Sudoku() == true)
         t.mostrarT();
@@ -17,5 +28,6 @@ int main()
         cout << "No solution exists";
     t.~Tabla();
     system("pause");
+    }
     return 0;
 }
